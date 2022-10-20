d7c9fc6dc645c3a5642f33a1af93f88b2c5cc3f6
hxiong <hxiong@ctrip.com>
2022-10-20 19:53:57 +0800

    update action doing

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ async function run() {
     const ms = core.getInput('milliseconds');
     core.info(`Waiting ${ms} milliseconds ...`);
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    core.info('start at ' + (new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
     await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    core.info('end at ' +(new Date()).toTimeString());
 
     core.setOutput('time', new Date().toTimeString());
   } catch (error) {
