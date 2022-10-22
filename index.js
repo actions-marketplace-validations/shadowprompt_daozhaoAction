dff05b6ff01feea97d68091173c5af00962f9a27
shadowprompt <huixiong.cn@gmail.com>
2022-10-23 07:03:20 +0800

    测试8

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ async function run() {
     await wait(parseInt(ms));
     core.info('end at=>' +(new Date()).toTimeString());
 
-    core.setOutput('time', 'abc');
+    core.setOutput('time', (new Date()).toTimeString());
   } catch (error) {
     core.setFailed(error.message);
   }
